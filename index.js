3f33d881bd171e92ad85549454faebd7d375de65
Harsh Gupta <harshhvg999@gmail.com>
2024-01-26 17:55:57 +0530

    3.1 to 3.8

---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+// const morganLog = morgan('tiny');
+
+const morganLog = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    JSON.stringify(req.body),
+  ].join(" ");
+});
+
+app.use(express.json());
+app.use(morganLog);
+
+let phoneBook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = () => {
+  const max = 5000;
+  return (Math.random() * max) | 0;
+};
+
+app.get("/", (request, response) => {
+  const count = phoneBook.length;
+
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const now = Date(new Date().toUTCString());
+
+  response.send(`<div>Phonebook has info for ${count} people</div>
+  ${now}`);
+});
+
+app.get("/api/persons/", (request, response) => {
+  response.json(phoneBook);
+});
+
+app.get("/api/persons/:id/", (request, response) => {
+  const id = Number(request.params.id);
+  const person = phoneBook.find((p) => p.id === id);
+  if (!person) {
+    response.status(404).end();
+  } else {
+    response.json(person);
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  phoneBook = phoneBook.filter((p) => p.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons/", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: "content missing" });
+  }
+  if (phoneBook.find((p) => p.name === body.name)) {
+    return response.status(400).json({ error: "name already exists" });
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  phoneBook = phoneBook.concat(person);
+  response.json(person);
+});
+
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+});
